51471451db43769b0020f9e7009a422e6762c293
Scorprog <99339505+DavidMasila@users.noreply.github.com>
2022-04-22 00:36:55 +0300

    Update 0-print_list.c

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,27 +1,24 @@
-#include <stdio.h>
 #include "lists.h"
+#include <stdio.h>
 
 /**
- * print_list - prints elements of a list.
- * @h: pointer to list.
- * Return: size_t, number of nodes in list.
+ * print_list - print elements of linkedlist
+ * @h: linked list
+ *
+ * Return: number of nodes
  */
 
 size_t print_list(const list_t *h)
 {
-	size_t n;
+	size_t nbr_nodes = 0;
+	const list_t *current = h;
 
-	n = 0;
-	while (h)
+	while (current != NULL)
 	{
-		if (h->str == NULL)
-			printf("[0] (nil)\n");
-
-		else
-			printf("[%d] %s\n", h->len, h->str);
-
-		n++;
-		h = h->next;
+		printf("[%d] %s\n", current->len,
+		current->str != NULL ? current->str : "(nil)");
+		current = current->next;
+		nbr_nodes++;
 	}
-	return (n);
+	return (nbr_nodes);
 }
